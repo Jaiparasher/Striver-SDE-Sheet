218520446155a21f1c762ac4df2d8d6f9c470893
Jai Parasher <jaiparasher30@gmail.com>
2024-01-15 21:16:36 +0530

    Day 6

---
+++ Day 6/4.Check if Linked List is Plaindrome.cpp
@@ -0,0 +1,37 @@
+//Hashing
+
+bool isPalindrome(node* head) {
+    vector<int> arr;
+    while(head != NULL) {
+        arr.push_back(head->num);
+        head = head->next;
+    }
+    for(int i=0;i<arr.size()/2;i++) 
+        if(arr[i] != arr[arr.size()-i-1]) return false;
+    return true;
+}
+
+//Optimized
+
+bool isPalindrome(node* head) {
+    if(head==NULL||head->next==NULL) return true;
+        
+    node* slow = head;
+    node* fast = head;
+        
+    while(fast->next!=NULL&&fast->next->next!=NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+        
+    slow->next = reverse(slow->next);
+    slow = slow->next;
+    node* dummy = head;
+        
+    while(slow!=NULL) {
+        if(dummy->num != slow->num) return false;
+        dummy = dummy->next;
+        slow = slow->next;
+    }
+    return true;
+}
