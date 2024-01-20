76dcbdca5b8541cd59030a6aacd84cff434278c0
Jai Parasher <jaiparasher30@gmail.com>
2024-01-20 21:50:39 +0530

    Day 10

---
+++ Day 10/Print All Permutations of a String & Array.cpp
@@ -0,0 +1,71 @@
+//Recursion
+
+using namespace std;
+class Solution {
+  private:
+    void recurPermute(vector < int > & ds, vector < int > & nums, vector < vector < int >> & ans, int freq[]) {
+      if (ds.size() == nums.size()) {
+        ans.push_back(ds);
+        return;
+      }
+      for (int i = 0; i < nums.size(); i++) {
+        if (!freq[i]) {
+          ds.push_back(nums[i]);
+          freq[i] = 1;
+          recurPermute(ds, nums, ans, freq);
+          freq[i] = 0;
+          ds.pop_back();
+        }
+      }
+    }
+  public:
+    vector < vector < int >> permute(vector < int > & nums) {
+      vector < vector < int >> ans;
+      vector < int > ds;
+      int freq[nums.size()];
+      for (int i = 0; i < nums.size(); i++) freq[i] = 0;
+      recurPermute(ds, nums, ans, freq);
+      return ans;
+    }
+};
+
+//Backtracking
+
+class Solution {
+    void solve(vector<int>& nums,vector<vector<int>>& ans,int index){
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            solve(nums,ans,index+1);
+            swap(nums[index],nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index=0;
+        solve(nums,ans,index);
+        return ans;
+    }
+};
+
+    void recurPermute(int index, vector < int > & nums, vector < vector < int >> & ans) {
+      if (index == nums.size()) {
+        ans.push_back(nums);
+        return;
+      }
+      for (int i = index; i < nums.size(); i++) {
+        swap(nums[index], nums[i]);
+        recurPermute(index + 1, nums, ans);
+        swap(nums[index], nums[i]);
+      }
+    }
+  public:
+    vector < vector < int >> permute(vector < int > & nums) {
+      vector < vector < int >> ans;
+      recurPermute(0, nums, ans);
+      return ans;
+    }
