3d26b7e07ea3267c1399ce9b1b7e8014d4d7280c
Jai Parasher <jaiparasher30@gmail.com>
2024-03-06 23:34:39 +0530

    Day 23

--- Day 23/3.BFS Traversal.cpp
+++ Day 23/3.BFS Traversal.cpp
@@ -1,13 +1,13 @@
-/* 
-	Time complexity: O(n + m). 
-	Space complexity: O(n). 
+/*
+    Time complexity: O(n + m).
+    Space complexity: O(n).
 
-	Where 'n' is the number of vertices in the input graph and 
+    Where 'n' is the number of vertices in the input graph and
    'm' is the number of edges in the input graph.
 */
 
-
-vector<int> bfsTraversal(int n, vector<vector<int>>& adj) {
+vector<int> bfsTraversal(int n, vector<vector<int>> &adj)
+{
     vector<int> res;
     queue<int> q;
 
@@ -20,7 +20,8 @@ vector<int> bfsTraversal(int n, vector<vector<int>>& adj) {
     // Mark vertex 0 as visited (1 indicates visited, 0 indicates not visited)
     vis[0] = 1;
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         // Get the front vertex from the queue
         int topVertex = q.front();
         q.pop();
@@ -29,9 +30,11 @@ vector<int> bfsTraversal(int n, vector<vector<int>>& adj) {
         res.push_back(topVertex);
 
         // Traverse all neighbors of the current vertex
-        for (auto neighbor : adj[topVertex]) {
+        for (auto neighbor : adj[topVertex])
+        {
             // If the neighbor is not visited, mark it as visited and enqueue it
-            if (!vis[neighbor]) {
+            if (!vis[neighbor])
+            {
                 vis[neighbor] = 1;
                 q.push(neighbor);
             }
